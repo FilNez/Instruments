b630528c9ea77a23a5e0ba8f3ae6fc9a2470d6e0
Filipp <flirox@rambler.ru>
2021-03-28 22:29:24 +0300

    Flashlight app works

--- app/src/main/java/com/example/instruments/Flashlight.java
+++ app/src/main/java/com/example/instruments/Flashlight.java
@@ -1,11 +1,15 @@
 package com.example.instruments;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -14,7 +18,6 @@ public class Flashlight extends AppCompatActivity {
     private ImageButton switchOff;
     private ImageButton switchOn;
     private TextView text;
-    Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +28,6 @@ public class Flashlight extends AppCompatActivity {
         switchOn = (ImageButton) findViewById(R.id.flashlight_is_off);
         text = (TextView) findViewById(R.id.flashlight_condition);
 
-        camera = Camera.open();
-        final Camera.Parameters parameters = camera.getParameters();
-
         switchOn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,9 +35,14 @@ public class Flashlight extends AppCompatActivity {
                 switchOff.setVisibility(View.VISIBLE);
                 switchOn.setVisibility(View.GONE);
 
-                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-                camera.setParameters(parameters);
-                camera.startPreview();
+                CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+                try {
+                    String cameraId = cameraManager.getCameraIdList()[0];
+                    cameraManager.setTorchMode(cameraId, true);
+                }
+                catch (CameraAccessException e)
+                {}
             }
         });
 
@@ -48,9 +53,14 @@ public class Flashlight extends AppCompatActivity {
                 switchOff.setVisibility(View.GONE);
                 switchOn.setVisibility(View.VISIBLE);
 
-                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
-                camera.setParameters(parameters);
-                camera.stopPreview();
+                CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+                try {
+                    String cameraId = cameraManager.getCameraIdList()[0];
+                    cameraManager.setTorchMode(cameraId, false);
+                }
+                catch (CameraAccessException e)
+                {}
             }
         });
     }
